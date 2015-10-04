03d2f26f824eb97c18aede9c23d2e6974e3f04cb
sakapun <sakapun5th@gmail.com>
2015-10-05 00:48:47 +0900

    5

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
   res.send('Hello, World!');
 });
 
-app.listen(80);
+app.listen(app.get('port'));
