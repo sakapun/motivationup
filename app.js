ccf0c357440dc763a73ca4bbd4fe81172255dd5c
sakapun <sakapun5th@gmail.com>
2015-10-05 02:08:15 +0900

    view pinterst

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
+var client = require('cheerio-httpcli');
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
 
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
-  res.send('Hello, World!');
+	client.fetch('https://jp.pinterest.com/search/pins/', { q: 'japanese+pottery' }, function (err, $) {
+		var img = $(".pinWrapper img").eq(0).attr('src');
+		var href = $(".pinWrapper a.pinImageWrapper").eq(0).attr('href');
+		res.render("index", {
+			link1: "https://jp.pinterest.com" + href,
+			img1: img});
+	});
 });
 
 app.listen(app.get('port'));
